e77d942967ffabfb6c52e8697bd5470a767a42a2
Anton Giertli <agiertli@redhat.com>
2024-09-05 17:39:18 +0000

    change hello msg

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "Christina") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "European Union") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
